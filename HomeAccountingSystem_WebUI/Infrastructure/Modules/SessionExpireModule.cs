25c78e50bc45e8c751d3bf0eae7411d34ceab80a
Susarev Andrey <aka.apostol@gmail.com>
2016-11-24 09:43:46 +0500

    Wrote logic in SessionExpireModule. If Currentnotification == RequestNotification.AcquireRequestState and session is null then we unauthorize user and redirect him to / url.

--- HomeAccountingSystem_WebUI/Infrastructure/Modules/SessionExpireModule.cs
+++ HomeAccountingSystem_WebUI/Infrastructure/Modules/SessionExpireModule.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 
 namespace HomeAccountingSystem_WebUI.Infrastructure.Modules
 {
     public class SessionExpireModule:IHttpModule
     {
-        public void Init(HttpApplication context)
+        public void Init(HttpApplication app)
         {
-            throw new NotImplementedException();
+            app.PostAcquireRequestState += HandleEvent;
+        }
+
+        private void HandleEvent(object sender, EventArgs e)
+        {
+            var ctx = HttpContext.Current;
+            if (ctx.CurrentNotification == RequestNotification.AcquireRequestState)
+            {
+                var session = ctx.Session;
+                if (session == null)
+                {
+                    var auth = ctx.GetOwinContext().Authentication;
+                    auth.SignOut();
+                    ctx.Response.Redirect("/", true);
+                }
+            }
         }
 
         public void Dispose()
         {
-            
+                
         }
     }
 }
