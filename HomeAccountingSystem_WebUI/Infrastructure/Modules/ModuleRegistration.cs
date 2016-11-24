faf7f37872051216021e4491fc0232557efb8599
Susarev Andrey <aka.apostol@gmail.com>
2016-11-24 09:53:17 +0500

    Added new class ModuleRegistration to register SessionExpiremodule in application through HttpApplication.RegisterModule.

---
+++ HomeAccountingSystem_WebUI/Infrastructure/Modules/ModuleRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using HomeAccountingSystem_WebUI.Infrastructure.Modules;
+
+[assembly: PreApplicationStartMethod(typeof(ModuleRegistration),"RegisterModule")]
+
+namespace HomeAccountingSystem_WebUI.Infrastructure.Modules
+{
+    public class ModuleRegistration
+    {
+        public static void RegisterModule()
+        {
+            HttpApplication.RegisterModule(typeof(SessionExpireModule));
+        }
+    }
+}
