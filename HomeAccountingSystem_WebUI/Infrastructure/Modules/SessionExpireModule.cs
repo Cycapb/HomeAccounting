c3689e402eabaae61f4a6ac7481f58303049c36b
Susarev Andrey <aka.apostol@gmail.com>
2016-11-24 08:53:32 +0500

    Created folder WebUI\Infrastructure\Modules to place httpmodules here. Added new class SessionExpireModule: IHttpModule

---
+++ HomeAccountingSystem_WebUI/Infrastructure/Modules/SessionExpireModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeAccountingSystem_WebUI.Infrastructure.Modules
+{
+    public class SessionExpireModule:IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
