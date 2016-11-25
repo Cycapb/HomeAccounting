6760ca853e24509f41d56bb70a03e93cceb0dc7a
Susarev Andrey <aka.apostol@gmail.com>
2016-11-25 14:18:36 +0500

    Added TimeModule to get time between request and response.

---
+++ HomeAccountingSystem_WebUI/Infrastructure/Modules/TimerModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+
+namespace HomeAccountingSystem_WebUI.Infrastructure.Modules
+{
+    public class TimerModule:IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
