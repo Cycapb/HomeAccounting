5e8622e7947ed44534398ef3896a8d8f32679072
Susarev Andrey <aka.apostol@gmail.com>
2016-11-25 14:30:30 +0500

    Added logic to TimerModule which get request's overall time.

--- HomeAccountingSystem_WebUI/Infrastructure/Modules/TimerModule.cs
+++ HomeAccountingSystem_WebUI/Infrastructure/Modules/TimerModule.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Web;
 
 namespace HomeAccountingSystem_WebUI.Infrastructure.Modules
 {
     public class TimerModule:IHttpModule
     {
+        private readonly Stopwatch _stopwatch;
         public void Init(HttpApplication context)
         {
-            
+            context.BeginRequest += Context_BeginRequest;
+            context.EndRequest += Context_EndRequest;
+        }
+
+        private void Context_EndRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.CurrentNotification == RequestNotification.EndRequest)
+            {
+                var elapsedTime = _stopwatch.ElapsedTicks / Stopwatch.Frequency;
+                HttpContext.Current.Response.Write($"<div class='alert alert-success'>Общее время: {elapsedTime}</div>");
+            }
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.CurrentNotification == RequestNotification.BeginRequest)
+            {
+               _stopwatch.Start();
+            }    
         }
 
         public void Dispose()
