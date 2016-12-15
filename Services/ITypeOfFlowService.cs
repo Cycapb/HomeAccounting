a0dcbebb294ba34cf2ce184031d6e619ed16a289
Susarev Andrey <aka.apostol@gmail.com>
2016-12-15 10:35:47 +0500

    Added type ITypeOfFlowService

---
+++ Services/ITypeOfFlowService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using HomeAccountingSystem_DAL.Model;
+
+namespace Services
+{
+    public interface ITypeOfFlowService
+    {
+        IEnumerable<TypeOfFlow> GetList();
+        IEnumerable<Category> GetCategories(int typeOfFlowId);
+    }
+}
