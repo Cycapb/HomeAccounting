4d04591e745680184a0970695ab6ddd822e4368c
Susarev Andrey <aka.apostol@gmail.com>
2016-12-15 10:38:11 +0500

    Implemented ITypeOfFlowService members in TypeOfFlowService

---
+++ BussinessLogic/Services/TypeOfFlowService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using HomeAccountingSystem_DAL.Model;
+using HomeAccountingSystem_DAL.Repositories;
+using Services;
+
+namespace BussinessLogic.Services
+{
+    public class TypeOfFlowService:ITypeOfFlowService
+    {
+        private readonly IRepository<TypeOfFlow> _tofRepository;
+
+        public TypeOfFlowService(IRepository<TypeOfFlow> tofRepository)
+        {
+            _tofRepository = tofRepository;
+        }
+
+        public IEnumerable<TypeOfFlow> GetList()
+        {
+            return _tofRepository.GetList();
+        }
+
+        public IEnumerable<Category> GetCategories(int typeOfFlowId)
+        {
+            return _tofRepository.GetItem(typeOfFlowId).Category;
+        }
+    }
+}
