a8ee9f7c8e4743c63dad5189b2d3b361e312da72
Susarev Andrey <aka.apostol@gmail.com>
2016-12-28 18:11:56 +0500

    Added method DeleteAsync to IDebtService and implemented it in DebtService. Added type DebtServiceTests. Wrote test for DebtService.DeleteAsync.

---
+++ HomeAccountingSystem_UnitTests/DebtServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using BussinessLogic.Services;
+using HomeAccountingSystem_DAL.Model;
+using HomeAccountingSystem_DAL.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Services;
+
+namespace HomeAccountingSystem_UnitTests
+{
+    [TestClass]
+    public class DebtServiceTests
+    {
+        private readonly Mock<IRepository<Debt>> _debtRepoMock;
+        private readonly Mock<IRepository<Account>> _accRepoMock;
+        private readonly IDebtService _service;
+
+        public DebtServiceTests()
+        {
+            _debtRepoMock = new Mock<IRepository<Debt>>();
+            _accRepoMock = new Mock<IRepository<Account>>();
+            _service = new DebtService(_debtRepoMock.Object, _accRepoMock.Object);
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync()
+        {
+            await _service.DeleteAsync(It.IsAny<int>());
+
+            _debtRepoMock.Verify(m => m.DeleteAsync(It.IsAny<int>()),Times.Exactly(1));
+        }
+    }
+}
