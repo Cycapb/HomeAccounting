9e69a9585efff61d3a210a630426de6e8c3be974
Susarev Andrey <aka.apostol@gmail.com>
2016-12-28 18:13:12 +0500

    Added type called DebtControllerTests.

---
+++ WebUI.Tests/DebtControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebUI.Tests
+{
+    [TestClass]
+    public class DebtControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
