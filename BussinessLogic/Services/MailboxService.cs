93451a4b03615ccd5552b9a4941f724c971ecb93
Susarev Andrey <aka.apostol@gmail.com>
2017-02-12 12:45:37 +0500

    Created new type IMailboxService to manage notification mail boxes for services. Implemented it in MailboxService. Created class for unit tests MailboxServiceTests.

---
+++ BussinessLogic/Services/MailboxService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModels.Model;
+using Services;
+
+namespace BussinessLogic.Services
+{
+    public class MailboxService : IMailboxService
+    {
+        public Task<NotificationMailBox> AddAsync(NotificationMailBox mailbox)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetItemAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NotificationMailBox>> GetListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IMailboxService.cs
@@ -0,0 +1,17 @@
+﻿using DomainModels.Model;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace Services
+{
+    /// <summary>
+    /// Служит для управления почтовыми ящиками системы уведомлений
+    /// </summary>
+    public interface IMailboxService
+    {
+        Task<NotificationMailBox> AddAsync(NotificationMailBox mailbox);
+        Task GetItemAsync(int id);
+        Task<IEnumerable<NotificationMailBox>> GetListAsync();
+        Task DeleteAsync(int id);
+        void SaveAsync();
+    }
+}
