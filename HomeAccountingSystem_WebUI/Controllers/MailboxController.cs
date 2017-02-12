72e7a865e754c08f3bac466e7c4e0f0fda623954
Susarev Andrey <aka.apostol@gmail.com>
2017-02-12 16:14:34 +0500

    Added MailboxController and implemented logic in it's Index action method.

---
+++ HomeAccountingSystem_WebUI/Controllers/MailboxController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services;
+using System.Threading.Tasks;
+
+namespace HomeAccountingSystem_WebUI.Controllers
+{
+    public class MailboxController : Controller
+    {
+        private IMailboxService _mailboxService;
+
+        public MailboxController(IMailboxService mailboxService)
+        {
+            _mailboxService = mailboxService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var mailboxes = await _mailboxService.GetListAsync();
+            return PartialView();
+        }
+    }
+}
