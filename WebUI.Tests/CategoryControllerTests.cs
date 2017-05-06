b609e8fd8968c2651bff29f2a90c9194dba4289f
Susarev Andrey <aka.apostol@gmail.com>
2017-05-06 22:08:49 +0500

    Wrote GetCategoriesAndPages_ReturnsPartialView

--- WebUI.Tests/CategoryControllerTests.cs
+++ WebUI.Tests/CategoryControllerTests.cs
@@ -8,6 +8,7 @@ using HomeAccountingSystem_WebUI.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Services;
+using System;
 
 namespace WebUI.Tests
 {
@@ -17,12 +18,14 @@ namespace WebUI.Tests
         private readonly Mock<ITypeOfFlowService> _tofService;
         private readonly Mock<ICategoryService> _catService;
         private readonly Mock<IPlanningHelper> _planningHelper;
+        private Mock<ICategoryHelper> _categoryHelper;
 
         public CategoryControllerTests()
         {
             _catService = new Mock<ICategoryService>();
             _tofService = new Mock<ITypeOfFlowService>();
             _planningHelper = new Mock<IPlanningHelper>();
+            _categoryHelper = new Mock<ICategoryHelper>();
         }
 
         [TestMethod]
@@ -63,12 +66,11 @@ namespace WebUI.Tests
 
         [TestMethod]
         [TestCategory("CategoryControllerTests")]
-        public async Task Can_Save_Valid_Category_Changes()
+        public async Task Edit_InputCategory_ValidModelState_ReturnsRedirectToAction()
         {
-            Category category = new Category() { CategoryID = 1, Name = "Test" };
-            CategoryController target = new CategoryController(_tofService.Object, null, _catService.Object);
+            CategoryController target = new CategoryController(_tofService.Object, null, _catService.Object, null);
 
-            var result = await target.Edit(category);
+            var result = await target.Edit(new Category());
 
             _catService.Verify(m => m.UpdateAsync(It.IsAny<Category>()), Times.Exactly(1));
             _catService.Verify(m => m.SaveAsync(), Times.Exactly(1));
@@ -77,20 +79,16 @@ namespace WebUI.Tests
 
         [TestMethod]
         [TestCategory("CategoryControllerTests")]
-        public async Task Cannot_Save_Invalid_Category_Changes()
+        public async Task Edit_InputCategory_InvalidModelState_ReturnsPartialViewResult()
         {
-            _tofService.Setup(m => m.GetList()).Returns(new List<TypeOfFlow>()
-            {
-                new TypeOfFlow() {TypeID = 1, TypeName = "Type1"},
-                new TypeOfFlow() {TypeID = 2, TypeName = "Type2"}
-            });
-            Category category = new Category() { CategoryID = 1, Name = "Test" };
-            CategoryController target = new CategoryController(_tofService.Object, null, _catService.Object);
+            CategoryController target = new CategoryController(_tofService.Object, null, _catService.Object, null);
             target.ModelState.AddModelError("error", "error");
 
-            var result = await target.Edit(category);
+            var result = await target.Edit(new Category());
+            var model = (result as PartialViewResult).ViewBag.TypesOfFlow;
 
-            _catService.Verify(m => m.SaveAsync(), Times.Never);
+            _tofService.Verify(m => m.GetListAsync(), Times.Exactly(1));
+            Assert.IsInstanceOfType(model, typeof(IEnumerable<TypeOfFlow>));
             Assert.IsInstanceOfType(result, typeof(PartialViewResult));
         }
 
@@ -98,7 +96,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Edit_CategoryNull_ReturnsRedirectToIndex()
         {
-            var target = new CategoryController(_tofService.Object, null, _catService.Object);
+            var target = new CategoryController(_tofService.Object, null, _catService.Object, null);
             _catService.Setup(m => m.GetItemAsync(It.IsAny<int>())).ReturnsAsync(null);
 
             var result = await target.Edit(It.IsAny<int>());
@@ -111,7 +109,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Edit_CategoryNotNull_ReturnsPartialView()
         {
-            var target = new CategoryController(_tofService.Object, null, _catService.Object);
+            var target = new CategoryController(_tofService.Object, null, _catService.Object, null);
             _catService.Setup(m => m.GetItemAsync(It.IsAny<int>())).ReturnsAsync(new Category());
 
             var result = await target.Edit(It.IsAny<int>());
@@ -123,7 +121,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Add_InputWebUser_ReturnsPartialView()
         {
-            var target = new CategoryController(_tofService.Object,null, null);
+            var target = new CategoryController(_tofService.Object,null, null, null);
 
             var result = await target.Add(new WebUser() { Id = "1" });
             var model = (result as PartialViewResult).ViewBag.TypesOfFlow;
@@ -137,7 +135,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Can_Add_Valid_Category_Rerurns_RedirectToAction()
         {
-            var target = new CategoryController(null, _planningHelper.Object, _catService.Object);
+            var target = new CategoryController(null, _planningHelper.Object, _catService.Object, null);
 
             var result = await target.Add(new WebUser() { Id = "1" }, new Category() { CategoryID = 1, Name = "Cat1" });
 
@@ -156,7 +154,7 @@ namespace WebUI.Tests
                 new TypeOfFlow() {TypeID = 1, TypeName = "Type1"},
                 new TypeOfFlow() {TypeID = 2, TypeName = "Type2"}
             });
-            var target = new CategoryController(_tofService.Object, _planningHelper.Object, null);
+            var target = new CategoryController(_tofService.Object, _planningHelper.Object, null, null);
             target.ModelState.AddModelError("error", "error");
 
             var result = await target.Add(new WebUser() { Id = "1" }, category);
@@ -169,7 +167,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Delete_CannotDeleteIfHasAnyDependencies()
         {
-            var target = new CategoryController(null, null, _catService.Object);
+            var target = new CategoryController(null, null, _catService.Object, null);
             _catService.Setup(m => m.HasDependencies(It.IsAny<int>())).ReturnsAsync(true);
 
             var result = await target.Delete(It.IsAny<int>());
@@ -181,7 +179,7 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task Delete_CanDeleteIfHasNoDependencies()
         {
-            var target = new CategoryController(null, null, _catService.Object);
+            var target = new CategoryController(null, null, _catService.Object, null);
             _catService.Setup(m => m.HasDependencies(It.IsAny<int>())).ReturnsAsync(false);
 
             var result = await target.Delete(It.IsAny<int>());
@@ -194,7 +192,12 @@ namespace WebUI.Tests
         [TestCategory("CategoryControllerTests")]
         public async Task GetCategoriesAndPages_ReturnsPartialView()
         {
+            var target = new CategoryController(null, null, null, _categoryHelper.Object);
+            _categoryHelper.Setup(m => m.CreateCategoriesViewModel(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<Func<Category, bool>>())).ReturnsAsync(new CategoriesViewModel());
 
+            var result = await target.GetCategoriesAndPages(new WebUser(), 1);
+
+            Assert.IsInstanceOfType(result, typeof(PartialViewResult));            
         }
     }
 }
