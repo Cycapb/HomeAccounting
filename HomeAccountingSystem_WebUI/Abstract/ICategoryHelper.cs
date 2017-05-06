3fd90d5d285e1a4bf60a5ef3e7d5282b519d68f3
Susarev Andrey <aka.apostol@gmail.com>
2017-05-06 17:07:07 +0500

    Added ICategoryHelper to the WebUI project.

---
+++ HomeAccountingSystem_WebUI/Abstract/ICategoryHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using HomeAccountingSystem_WebUI.Models;
+using DomainModels.Model;
+
+namespace HomeAccountingSystem_WebUI.Abstract
+{
+    public interface ICategoryHelper
+    {
+        CategoriesViewModel CreateCategoriesViewModel(int page, int itemsPerPage, Action<Category> cat);
+    }
+}
