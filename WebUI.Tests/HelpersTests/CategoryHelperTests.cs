1297895fad62d84fcc35545a7e549ac9ba470111
Susarev Andrey <aka.apostol@gmail.com>
2017-05-15 15:46:42 +0500

    Started to write unit tests for CategoryHelper.Created CategoryHelperTest class. #HA-42 Developing

---
+++ WebUI.Tests/HelpersTests/CategoryHelperTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using Moq;
+using HomeAccountingSystem_WebUI.Helpers;
+using DomainModels.Model;
+using System.Collections.Generic;
+
+namespace WebUI.Tests.HelpersTests
+{
+    [TestClass]
+    public class CategoryHelperTests
+    {
+        private readonly Mock<ICategoryService> _categoryService;
+        
+        private List<Category> categories => new List<Category>()
+        {
+            new Category(){ Name = "C1", UserId = "1"},
+            new Category(){ Name = "C2", UserId = "2"},
+            new Category(){ Name = "C3", UserId = "2"},
+            new Category(){ Name = "C4", UserId = "2"},
+            new Category(){ Name = "C5", UserId = "1"},
+        };
+
+        public CategoryHelperTests()
+        {
+            _categoryService = new Mock<ICategoryService>();            
+        }
+        
+        [TestCategory("CategoryHelperTests")]
+        [TestMethod]
+        public async Task GetCategoriesToShowOnPage()
+        {
+
+            _categoryService.Setup(m => m.GetListAsync()).ReturnsAsync(categories);
+            var target = new CategoryHelper(_categoryService.Object);
+
+            var result = (await target.GetCategoriesToShowOnPage(1, 1, CheckByUserId));
+            
+            //ToDo Write assertions
+
+            bool CheckByUserId(Category category)
+            {
+                if (category.UserId == "1")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
