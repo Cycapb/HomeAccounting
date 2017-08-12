f2f5509d6bfd57d610a5234c8378a789c5e3c11f
Susarev Andrey <aka.apostol@gmail.com>
2017-08-12 16:32:52 +0500

    Installed Moq, EntityFramework and added references to DomainModels #HA-52 Developing

---
+++ DomainModels.EntityORM.Tests/EntityRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainModels.Model;
+
+namespace DomainModels.EntityORM.Tests
+{
+    [TestClass]
+    public class EntityRepositoryTests
+    {
+        [TestMethod]
+        [TestCategory("EntityRepositoryTests")]
+        public void GetListRaiseEception()
+        {
+            
+            var repo = new EntityRepository<Account>();
+            
+        }
+    }
+}
