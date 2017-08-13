627917183a978300b16b32f4f90234ce0b160868
Susarev Andrey <aka.apostol@gmail.com>
2017-08-13 18:51:18 +0500

    Added new exception type called ServiceExceptionto to BussinessLogic project #HA-52 Developing

---
+++ BussinessLogic/Exceptions/ServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BussinessLogic.Exceptions
+{
+    public class ServiceException:Exception
+    {
+        public ServiceException(string message, Exception innerException):base(message, innerException)
+        {
+            
+        }
+    }
+}
