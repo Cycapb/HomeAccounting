7168bb9b9fb62339b36535ac9137b5a7577b183d
Susarev Andrey <aka.apostol@gmail.com>
2017-08-16 00:11:03 +0500

    Added try/catch to all methods in DbHelper type #HA-52 Developing

--- BussinessLogic/Services/DbHelper.cs
+++ BussinessLogic/Services/DbHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using DomainModels.Model;
 using DomainModels.Repositories;
+using DomainModels.Exceptions;
+using Services.Exceptions;
 using Services;
 
 namespace BussinessLogic.Services
@@ -19,135 +21,213 @@ namespace BussinessLogic.Services
             _pItemRepo = pItemRepo;
         }
 
-        #region Methods for the WEB
-
-        //--------Here are the methods for the WEB--------------------
         public IEnumerable<PayingItem> GetPayingItemsInDatesWeb(DateTime dateFrom, DateTime dateTo, IWorkingUser user)
-        {            
-            return _pItemRepo.GetList()
-                .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date) && d.UserId == user.Id)
-                .OrderBy(d => d.Date)
-                .ToList();
+        {
+            try
+            {
+                return _pItemRepo.GetList()
+                    .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date) && d.UserId == user.Id)
+                    .OrderBy(d => d.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetPayingItemsInDatesWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayingItem> GetPayingItemsByDateWeb(DateTime date, IWorkingUser user)
-        {            
-            return _pItemRepo.GetList()
-                .Where(d => d.Date == date.Date && d.UserId == user.Id)
-                .ToList();
+        {
+            try
+            {
+                return _pItemRepo.GetList()
+                    .Where(d => d.Date == date.Date && d.UserId == user.Id)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetPayingItemsByDateWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayItem> GetPayItemsByDateWeb(DateTime date, IWorkingUser user)
-        {            
-            return (from pItem in _pItemRepo.GetList()
-                where pItem.UserId == user.Id
-                select new PayItem()
-                {
-                    AccountName = pItem.Account.AccountName,
-                    CategoryName = pItem.Category.Name,
-                    Comment = pItem.Comment,
-                    Summ = pItem.Summ,
-                    Date = pItem.Date,
-                    ItemId = pItem.ItemID
-                })
-                .Where(d => d.Date == date.Date)
-                .ToList();                        
+        {
+            try
+            {
+                return (from pItem in _pItemRepo.GetList()
+                        where pItem.UserId == user.Id
+                        select new PayItem()
+                        {
+                            AccountName = pItem.Account.AccountName,
+                            CategoryName = pItem.Category.Name,
+                            Comment = pItem.Comment,
+                            Summ = pItem.Summ,
+                            Date = pItem.Date,
+                            ItemId = pItem.ItemID
+                        })
+                    .Where(d => d.Date == date.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetPayingItemsByDateWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayItem> GetPayItemsInDatesWeb(DateTime dateFrom, DateTime dateTo, IWorkingUser user)
-        {            
-            return (from pItem in _pItemRepo.GetList()
-                where pItem.UserId == user.Id
-                select new PayItem()
-                {
-                    AccountName = pItem.Account.AccountName,
-                    CategoryName = pItem.Category.Name,
-                    Comment = pItem.Comment,
-                    Summ = pItem.Summ,
-                    Date = pItem.Date,
-                    ItemId = pItem.ItemID,
-                    TypeOfFlowId = pItem.Category.TypeOfFlowID
-                })
-                .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
-                .OrderBy(d => d.Date)
-                .ToList();
+        {
+            try
+            {
+                return (from pItem in _pItemRepo.GetList()
+                        where pItem.UserId == user.Id
+                        select new PayItem()
+                        {
+                            AccountName = pItem.Account.AccountName,
+                            CategoryName = pItem.Category.Name,
+                            Comment = pItem.Comment,
+                            Summ = pItem.Summ,
+                            Date = pItem.Date,
+                            ItemId = pItem.ItemID,
+                            TypeOfFlowId = pItem.Category.TypeOfFlowID
+                        })
+                    .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
+                    .OrderBy(d => d.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetPayItemsInDatesWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayItem> GetCategoryPayItemsByDateWeb(DateTime date, int categoryId, IWorkingUser user)
         {
-            return (from pItem in _pItemRepo.GetList()
-                where pItem.CategoryID == categoryId && pItem.UserId == user.Id
-                select new PayItem()
-                {
-                    AccountName = pItem.Account.AccountName,
-                    CategoryName = pItem.Category.Name,
-                    Comment = pItem.Comment,
-                    Summ = pItem.Summ,
-                    Date = pItem.Date,
-                    ItemId = pItem.ItemID
-                })
-                .Where(d => d.Date == date.Date)
-                .ToList();
+            try
+            {
+                return (from pItem in _pItemRepo.GetList()
+                        where pItem.CategoryID == categoryId && pItem.UserId == user.Id
+                        select new PayItem()
+                        {
+                            AccountName = pItem.Account.AccountName,
+                            CategoryName = pItem.Category.Name,
+                            Comment = pItem.Comment,
+                            Summ = pItem.Summ,
+                            Date = pItem.Date,
+                            ItemId = pItem.ItemID
+                        })
+                    .Where(d => d.Date == date.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetCategoryPayItemsByDateWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayItem> GetCategoryPayItemsInDatesWeb(DateTime dateFrom, DateTime dateTo, int categoryId,
             IWorkingUser user)
         {
-            return  (from pItem in _pItemRepo.GetList()
-                where pItem.CategoryID == categoryId && pItem.UserId == user.Id
-                select new PayItem()
-                {
-                    AccountName = pItem.Account.AccountName,
-                    CategoryName = pItem.Category.Name,
-                    Comment = pItem.Comment,
-                    Summ = pItem.Summ,
-                    Date = pItem.Date,
-                    ItemId = pItem.ItemID
-                })
-                .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
-                .OrderBy(d => d.Date)
-                .ToList();
+            try
+            {
+                return (from pItem in _pItemRepo.GetList()
+                        where pItem.CategoryID == categoryId && pItem.UserId == user.Id
+                        select new PayItem()
+                        {
+                            AccountName = pItem.Account.AccountName,
+                            CategoryName = pItem.Category.Name,
+                            Comment = pItem.Comment,
+                            Summ = pItem.Summ,
+                            Date = pItem.Date,
+                            ItemId = pItem.ItemID
+                        })
+                    .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
+                    .OrderBy(d => d.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetCategoryPayItemsInDatesWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public IEnumerable<PayItem> GetPayItemsInDatesByTypeOfFlowWeb(DateTime dateFrom, DateTime dateTo,
             int typeOfFlowId, IWorkingUser user)
         {
-            return (from pItem in _pItemRepo.GetList()
-                where pItem.Category.TypeOfFlowID == typeOfFlowId && pItem.UserId == user.Id
-                select new PayItem()
-                {
-                    AccountName = pItem.Account.AccountName,
-                    CategoryName = pItem.Category.Name,
-                    Comment = pItem.Comment,
-                    Summ = pItem.Summ,
-                    Date = pItem.Date,
-                    ItemId = pItem.ItemID
-                })
-                .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
-                .OrderBy(d => d.Date)
-                .ToList();
+            try
+            {
+                return (from pItem in _pItemRepo.GetList()
+                        where pItem.Category.TypeOfFlowID == typeOfFlowId && pItem.UserId == user.Id
+                        select new PayItem()
+                        {
+                            AccountName = pItem.Account.AccountName,
+                            CategoryName = pItem.Category.Name,
+                            Comment = pItem.Comment,
+                            Summ = pItem.Summ,
+                            Date = pItem.Date,
+                            ItemId = pItem.ItemID
+                        })
+                    .Where(d => (d.Date >= dateFrom.Date) && (d.Date <= dateTo.Date))
+                    .OrderBy(d => d.Date)
+                    .ToList();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException(
+                    $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetPayItemsInDatesByTypeOfFlowWeb)} при обращении к БД",
+                    e);
+            }
         }
 
         public Task<string> GetBudgetOverAllWeb(IWorkingUser user)
         {
             return Task.Run(() =>
             {
-                return _accRepo.GetList()
-                    .Where(i => i.UserId == user.Id)
-                    .Sum(s => s.Cash)
-                    .ToString("c");
+                try
+                {
+                    return _accRepo.GetList()
+                        .Where(i => i.UserId == user.Id)
+                        .Sum(s => s.Cash)
+                        .ToString("c");
+                }
+                catch (DomainModelsException e)
+                {
+                    throw new ServiceException(
+                        $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetBudgetOverAllWeb)} при обращении к БД",
+                        e);
+                }
             });
         }
 
         public Task<string> GetBudgetInFactWeb(IWorkingUser user)
         {
             return Task.Run(() =>
-            {
-                return _accRepo.GetList()
-                 .Where(b => b.Use == true && b.UserId == user.Id)
-                 .Sum(s => s.Cash)
-                 .ToString("c");
-            }
+                {
+                    try
+                    {
+                        return _accRepo.GetList()
+                            .Where(b => b.Use == true && b.UserId == user.Id)
+                            .Sum(s => s.Cash)
+                            .ToString("c");
+                    }
+                    catch (DomainModelsException e)
+                    {
+                        throw new ServiceException(
+                            $"Ошибка в сервисе {nameof(DbHelper)} в методе {nameof(GetBudgetInFactWeb)} при обращении к БД",
+                            e);
+                    }
+                }
             );
         }
 
@@ -171,8 +251,5 @@ namespace BussinessLogic.Services
             .Where(i => i.Date == DateTime.Today)
             .Sum(i => i.Summ);
         }
-
-        #endregion
-
     }
 }
