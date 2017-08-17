b4e77b731a7b430971f2a41e87077db4e44f807c
Susarev Andrey <aka.apostol@gmail.com>
2017-08-17 11:09:43 +0500

    Added parameterless constructor to ServiceException type. Repaired unit-test AddAsync_ThrowsServiceException in MailboxServiceTests.

--- Services/Exceptions/ServiceException.cs
+++ Services/Exceptions/ServiceException.cs
@@ -4,6 +4,11 @@ namespace Services.Exceptions
 {
     public class ServiceException:Exception
     {
+        public ServiceException():base("Возникла ошибка в сервисе")
+        {
+            
+        }
+
         public ServiceException(string message, Exception innerException):base(message, innerException)
         {
             
