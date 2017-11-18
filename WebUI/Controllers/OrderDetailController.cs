f3f7bd9afb38a72f7fc94c12c757721f87171f6c
Susarev Andrey <aka.apostol@gmail.com>
2017-11-18 15:32:55 +0500

    Added try/catch blocks to all actions of the OrderDetailController. #HA-52 Developing

--- WebUI/Controllers/OrderDetailController.cs
+++ WebUI/Controllers/OrderDetailController.cs
@@ -8,6 +8,8 @@ using System.Web.SessionState;
 using DomainModels.Model;
 using WebUI.Models;
 using Services;
+using Services.Exceptions;
+using WebUI.Exceptions;
 
 namespace WebUI.Controllers
 {
@@ -27,28 +29,51 @@ namespace WebUI.Controllers
         [HttpPost]
         public async Task<ActionResult> Delete(int id)
         {
-            var orderId = (await _orderDetailService.GetItemAsync(id)).OrderId;
-            await _orderDetailService.DeleteAsync(id);
+            int orderId;
+            try
+            {
+                orderId = (await _orderDetailService.GetItemAsync(id)).OrderId;
+                await _orderDetailService.DeleteAsync(id);
+            }
+            catch (ServiceException e)
+            {
+                throw new WebUiException($"Ошибка в контроллере {nameof(OrderDetailController)} в методе {nameof(Delete)}", e);
+            }
             return RedirectToAction("Edit", "Order", new {id = orderId});
         }
 
         public async Task<ActionResult> Add(WebUser user, int id)
         {
-            var categories = (await GetCategories(user.Id)).ToList();
-            HttpContext.Cache.Insert("Categories", categories, null, Cache.NoAbsoluteExpiration, TimeSpan.FromSeconds(60));
-            var model = new AddOrderDetailView()
+            AddOrderDetailView model;
+            try
             {
-                OrderId = id,
-                Categories = categories,
-                Products = categories.FirstOrDefault()?.Product
-            };
+                var categories = (await GetCategories(user.Id)).ToList();
+                HttpContext.Cache.Insert("Categories", categories, null, Cache.NoAbsoluteExpiration, TimeSpan.FromSeconds(60));
+                model = new AddOrderDetailView()
+                {
+                    OrderId = id,
+                    Categories = categories,
+                    Products = categories.FirstOrDefault()?.Product
+                };
+            }
+            catch (ServiceException e)
+            {
+                throw new WebUiException($"Ошибка в контроллере {nameof(OrderDetailController)} в методе {nameof(Add)}", e);
+            }
             return PartialView("_Add", model);
         }
 
         [HttpPost]
         public async Task<ActionResult> Add(OrderDetail orderDetail)
         {
-            await _orderDetailService.CreateAsync(orderDetail);
+            try
+            {
+                await _orderDetailService.CreateAsync(orderDetail);
+            }
+            catch (ServiceException e)
+            {
+                throw new WebUiException($"Ошибка в контроллере {nameof(OrderDetailController)} в методе {nameof(Add)}", e);
+            }
             return RedirectToAction("Edit", "Order", new {id = orderDetail.OrderId});
         }
 
@@ -58,7 +83,14 @@ namespace WebUI.Controllers
             if (categories == null)
             {
                 var user = (WebUser)HttpContext.Session?["WebUser"];
-                categories = await GetCategories(user.Id);
+                try
+                {
+                    categories = await GetCategories(user.Id);
+                }
+                catch (ServiceException e)
+                {
+                    throw new WebUiException($"Ошибка в контроллере {nameof(OrderDetailController)} в методе {nameof(GetSubCategories)}", e);
+                }
             }
             var products = categories.FirstOrDefault(x => x.CategoryID == id)?.Product.OrderBy(x => x.ProductName);
             return PartialView("_SubCategories", products);
