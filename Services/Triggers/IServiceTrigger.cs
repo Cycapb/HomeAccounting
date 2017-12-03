e8f4921bfe7a9fd55573eb5bb29b97393345d682
Susarev Andrey <aka.apostol@gmail.com>
2017-12-03 20:33:45 +0500

    Added new typr IServiceTrigger to Services assembly. #HA-43 Developing

---
+++ Services/Triggers/IServiceTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.Triggers
+{
+    /// <summary>
+    /// Представляет собой замену триггерам в базе данных
+    /// </summary>
+    /// <typeparam name="T">Любой тип сущности из базы данных</typeparam>
+    public interface IServiceTrigger<in T>
+    {
+        /// <summary>
+        /// Срабатывает при создании записи в базе данных
+        /// </summary>
+        /// <param name="insertedItem">Создаваемая сущность</param>
+        /// <returns>Задачу</returns>
+        Task Insert(T insertedItem);
+        /// <summary>
+        /// Срабатывает при удалении записи из базы данных
+        /// </summary>
+        /// <param name="deletedItem">Удаляемая сущность</param>
+        /// <returns>Задачу</returns>
+        Task Delete(T deletedItem);
+        /// <summary>
+        /// Срабатывает при обновлении записи в базе данных
+        /// </summary>
+        /// <param name="updatedItem">Обновляемая сущность</param>
+        /// <returns>Задачу</returns>
+        Task Update(T updatedItem);
+    }
+}
