502f15368389ad65693634e498489e33dcdc7a95
Susarev Andrey <aka.apostol@gmail.com>
2018-01-05 14:03:59 +0500

    Реализовал интерфейс ICachemanager в MemoryCacheManager #HA-68 Developing

---
+++ BussinessLogic/Services/Caching/MemoryCacheManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Caching;
+using Loggers;
+using Services.Caching;
+
+namespace BussinessLogic.Services.Caching
+{
+    public class MemoryCacheManager:ICacheManager
+    {
+        private readonly Cache _cache;
+        private readonly IExceptionLogger _exceptionLogger;
+
+        public MemoryCacheManager(Cache cache, IExceptionLogger exceptionLogger)
+        {
+            _exceptionLogger = exceptionLogger;
+            _cache = cache;
+        }
+
+        public void Set(string key, object value)
+        {
+            try
+            {
+                _cache.Insert(key, value);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Возникла ошибка при помещении объекта в кэш в типе {nameof(MemoryCacheManager)} в методе {nameof(Set)}", e);
+            }
+        }
+
+        public object Get(string key)
+        {
+            try
+            {
+                return _cache.Get(key);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Возникла ошибка при получении объекта из кэша в типе {nameof(MemoryCacheManager)} в методе {nameof(Get)}", e);
+            }
+        }
+
+        public bool Remove(string key)
+        {
+            try
+            {
+                _cache.Remove(key);
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Возникла ошибка при удалении объекта из кэша в типе {nameof(MemoryCacheManager)} в методе {nameof(Get)}", e);
+            }
+        }
+    }
+}
--- Services/Caching/ICacheManager.cs
+++ Services/Caching/ICacheManager.cs
@@ -2,8 +2,8 @@
 {
     public interface ICacheManager
     {
-        void Set(object key, object value);
-        object Get(object key);
-        bool Remove(object key);
+        void Set(string key, object value);
+        object Get(string key);
+        bool Remove(string key);
     }
 }
