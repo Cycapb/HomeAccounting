b9bdb68a299af1298073c02bd414db4f26177a33
Susarev Andrey <aka.apostol@gmail.com>
2018-02-04 15:13:58 +0500

    Добавил интетерфейс ICreateCloseDebtService и сделал его реализацию CreateCloseDebtService. #HA-74 Developing

---
+++ BussinessLogic/Services/CreateCloseDebtService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+using DomainModels.Exceptions;
+using DomainModels.Model;
+using DomainModels.Repositories;
+using Services;
+using Services.Exceptions;
+
+namespace BussinessLogic.Services
+{
+    public class CreateCloseDebtService:ICreateCloseDebtService
+    {
+        private readonly IRepository<Debt> _debtRepo;
+        private readonly IRepository<Account> _accRepo;
+
+        public CreateCloseDebtService(IRepository<Debt> debtRepo, IRepository<Account> accRepo)
+        {
+            _debtRepo = debtRepo;
+            _accRepo = accRepo;
+        }
+
+        public Task CreateAsync(Debt debt)
+        {
+            try
+            {
+                return Task.Run(async () =>
+                {
+                    await _debtRepo.CreateAsync(debt);
+                    await _debtRepo.SaveAsync();
+                    await CreateDebtAsync(debt);
+                });
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException($"Ошибка в сервисе {nameof(CreateCloseDebtService)} в методе {nameof(CreateAsync)} при обращении к БД", e);
+            }
+        }
+
+        public async Task CloseAsync(int id)
+        {
+            try
+            {
+                var item = await _debtRepo.GetItemAsync(id);
+                item.DateEnd = DateTime.Now;
+                await _debtRepo.UpdateAsync(item);
+                await _debtRepo.SaveAsync();
+                await CloseDebtAsync(item);
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException($"Ошибка в сервисе {nameof(CreateCloseDebtService)} в методе {nameof(CloseAsync)} при обращении к БД", e);
+            }
+        }
+
+        private async Task CreateDebtAsync(Debt debt)
+        {
+            try
+            {
+                var acc = await _accRepo.GetItemAsync(debt.AccountId);
+                if (debt.TypeOfFlowId == 1)
+                {
+                    acc.Cash += debt.Summ;
+                }
+                else
+                {
+                    acc.Cash -= debt.Summ;
+                }
+                await _accRepo.UpdateAsync(acc);
+                await _accRepo.SaveAsync();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException($"Ошибка в сервисе {nameof(CreateCloseDebtService)} в методе {nameof(CreateDebtAsync)} при обращении к БД", e);
+            }
+        }
+
+        private async Task CloseDebtAsync(Debt debt)
+        {
+            try
+            {
+                var acc = await _accRepo.GetItemAsync(debt.AccountId);
+                if (debt.TypeOfFlowId == 1)
+                {
+                    acc.Cash -= debt.Summ;
+                }
+                else
+                {
+                    acc.Cash += debt.Summ;
+                }
+                await _accRepo.UpdateAsync(acc);
+                await _accRepo.SaveAsync();
+            }
+            catch (DomainModelsException e)
+            {
+                throw new ServiceException($"Ошибка в сервисе {nameof(CreateCloseDebtService)} в методе {nameof(CloseDebtAsync)} при обращении к БД", e);
+            }
+        }
+    }
+}
---
+++ Services/ICreateCloseDebtService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using DomainModels.Model;
+
+namespace Services
+{
+    public interface ICreateCloseDebtService
+    {
+        Task CreateAsync(Debt debt);
+        Task CloseAsync(int id);
+    }
+}
