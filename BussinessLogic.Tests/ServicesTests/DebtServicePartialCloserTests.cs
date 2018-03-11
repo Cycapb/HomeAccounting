c7b4cfa6f0655cc5200c963715393e7268ad2ccf
Susarev Andrey <aka.apostol@gmail.com>
2018-03-11 14:41:13 +0500

    Добавил логику добавления платежной транзакции при частичном закрытии долга и написал тесты. #HA-76 Developing

--- BussinessLogic.Tests/ServicesTests/DebtServicePartialCloserTests.cs
+++ BussinessLogic.Tests/ServicesTests/DebtServicePartialCloserTests.cs
@@ -13,14 +13,22 @@ namespace BussinessLogic.Tests.ServicesTests
     public class DebtServicePartialCloserTests
     {
         private List<Debt> _listOfDebts;
+        private List<Category> _listOfCategories;
         private readonly Mock<IRepository<Debt>> _debtRepositoryMock;
         private readonly DebtServicePartialCloser _debtServicePartialCloser;
+        private readonly Mock<IRepository<PayingItem>> _payingItemRepositoryMock;
+        private readonly Mock<IRepository<Category>> _categoryRepositoryMock;
 
         public DebtServicePartialCloserTests()
         {
             InitializeDebts();
+            InitializeCategories();
             _debtRepositoryMock = new Mock<IRepository<Debt>>();
-            _debtServicePartialCloser = new DebtServicePartialCloser(_debtRepositoryMock.Object);
+            _payingItemRepositoryMock = new Mock<IRepository<PayingItem>>();
+            _categoryRepositoryMock = new Mock<IRepository<Category>>();
+            _debtServicePartialCloser = new DebtServicePartialCloser(_debtRepositoryMock.Object, 
+                _payingItemRepositoryMock.Object, 
+                _categoryRepositoryMock.Object);
         }
 
         [TestMethod]
@@ -28,6 +36,7 @@ namespace BussinessLogic.Tests.ServicesTests
         public async Task CloseAsync_InputSum200_DebtWas500_Becomes300()
         {
             _debtRepositoryMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(_listOfDebts[0]);
+            _categoryRepositoryMock.Setup(x => x.GetListAsync()).ReturnsAsync(_listOfCategories);
 
             await _debtServicePartialCloser.CloseAsync(It.IsAny<int>(), 200);
 
@@ -40,10 +49,42 @@ namespace BussinessLogic.Tests.ServicesTests
         public async Task CloseAsync_InputSum600_DebtWas500_ThrowsArgumentOutOfrange()
         {
             _debtRepositoryMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(_listOfDebts[0]);
+            _categoryRepositoryMock.Setup(x => x.GetListAsync()).ReturnsAsync(_listOfCategories);
 
             await _debtServicePartialCloser.CloseAsync(It.IsAny<int>(), 600);
         }
 
+        [TestMethod]
+        [TestCategory("DebtServicePartialCloser")]
+        public async Task CloseAsync_CreatesPayingItem()
+        {
+            PayingItem payingItem = null;
+            _debtRepositoryMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(_listOfDebts[0]);
+            _categoryRepositoryMock.Setup(x => x.GetListAsync()).ReturnsAsync(_listOfCategories);
+            _payingItemRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<PayingItem>()))
+                .ReturnsAsync(new PayingItem())
+                .Callback<PayingItem>(x => payingItem = x);
+
+            await _debtServicePartialCloser.CloseAsync(It.IsAny<int>(), 300);
+
+            Assert.IsNotNull(payingItem);
+        }
+
+        [TestMethod]
+        [TestCategory("DebtServicePartialCloser")]
+        public async Task CloseAsync_InputSum300_CreatesPayingItemWithSumm300()
+        {
+            PayingItem payingItem = null;
+            _debtRepositoryMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(_listOfDebts[0]);
+            _categoryRepositoryMock.Setup(x => x.GetListAsync()).ReturnsAsync(_listOfCategories);
+            _payingItemRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<PayingItem>()))
+                .ReturnsAsync(new PayingItem())
+                .Callback<PayingItem>(x => payingItem = x);
+
+            await _debtServicePartialCloser.CloseAsync(It.IsAny<int>(), 300);
+
+            Assert.AreEqual(300, payingItem.Summ);
+        }
         private void InitializeDebts()
         {
             _listOfDebts = new List<Debt>()
@@ -54,6 +95,20 @@ namespace BussinessLogic.Tests.ServicesTests
                     DebtID = 1,
                     Summ = 500,
                     UserId = "1"
+                }
+            };
+        }
+
+        private void InitializeCategories()
+        {
+            _listOfCategories = new List<Category>()
+            {
+                new Category()
+                {
+                    CategoryID = 1,
+                    TypeOfFlowID = 1,
+                    UserId = "1",
+                    Name = "Долг"
                 }
             };
         }
--- BussinessLogic/Services/DebtServicePartialCloser.cs
+++ BussinessLogic/Services/DebtServicePartialCloser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DomainModels.Model;
 using DomainModels.Repositories;
@@ -9,10 +10,16 @@ namespace BussinessLogic.Services
     public class DebtServicePartialCloser:IDebtServicePartialCloser
     {
         private readonly IRepository<Debt> _debtRepository;
+        private readonly IRepository<PayingItem> _payingItemRepository;
+        private readonly IRepository<Category> _categoryRepository;
 
-        public DebtServicePartialCloser(IRepository<Debt> debtRepository)
+        public DebtServicePartialCloser(IRepository<Debt> debtRepository, 
+            IRepository<PayingItem> payingItemRepository, 
+            IRepository<Category> categoryRepository)
         {
             _debtRepository = debtRepository;
+            _payingItemRepository = payingItemRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public async Task CloseAsync(int debtId, decimal sum)
@@ -25,6 +32,34 @@ namespace BussinessLogic.Services
             debt.Summ -= sum;
             await _debtRepository.UpdateAsync(debt);
             await _debtRepository.SaveAsync();
+            await CreatePayingItem(debt, sum);
+        }
+
+        private async Task CreatePayingItem(Debt debt, decimal sum)
+        {
+            var typeOfFlowId = debt.TypeOfFlowId == 1 ? 2 : 1;
+            var categoryId = await GetClosedDebtCategoryId(debt.UserId, typeOfFlowId);
+            var payingItem = new PayingItem()
+            {
+                Summ = sum,
+                AccountID = debt.AccountId,
+                Date = debt.DateEnd ?? DateTime.Now,
+                Comment = debt.TypeOfFlowId == 1 ? "Частично закрыл свой долг" : "Мне частично вернули долг",
+                UserId = debt.UserId,
+                CategoryID = categoryId
+            };
+
+            await _payingItemRepository.CreateAsync(payingItem);
+            await _payingItemRepository.SaveAsync();
+        }
+
+        private async Task<int> GetClosedDebtCategoryId(string userId, int typeOfFlowId)
+        {
+            var category =
+                (await _categoryRepository.GetListAsync())
+                .Where(x => x.UserId == userId && x.TypeOfFlowID == typeOfFlowId)
+                .FirstOrDefault(c => c.Name.ToLower().Contains("Долг".ToLower()));
+            return category.CategoryID;
         }
     }
 }
