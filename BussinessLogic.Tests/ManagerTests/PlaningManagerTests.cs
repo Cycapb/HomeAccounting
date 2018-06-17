22eea6adc1df5714b9b43265906bc68f2d910da5
Susarev Andrey <aka.apostol@gmail.com>
2018-06-17 23:25:12 +0500

    Реализовал логику в методе ClosePlaningPeriod для закрытия предыдущего периода. #HA-82 Developing

---
+++ BussinessLogic.Tests/ManagerTests/PlaningManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using BussinessLogic.Managers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Services;
+using Services.Exceptions;
+
+namespace BussinessLogic.Tests.ManagerTests
+{
+    [TestClass]
+    public class PlaningManagerTests
+    {
+        private readonly Mock<IPlanItemService> _planItemServiceMoq;
+
+        public PlaningManagerTests()
+        {
+            _planItemServiceMoq = new Mock<IPlanItemService>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceException))]
+        public async Task ClosePlaningPeriod_ThrowsServiceException()
+        {
+            _planItemServiceMoq.Setup(m => m.GetListAsync(It.IsAny<string>())).ThrowsAsync(new ServiceException());
+            var target = new PlaningManager(_planItemServiceMoq.Object);
+
+            await target.ClosePlaningPeriod(It.IsAny<string>(), It.IsAny<DateTime>());
+        }
+
+        [TestMethod]
+        public async Task ClosePlaningPeriod_ThrowsServiceException_WithInnerServiceException()
+        {
+            _planItemServiceMoq.Setup(m => m.GetListAsync(It.IsAny<string>())).ThrowsAsync(new ServiceException());
+            var target = new PlaningManager(_planItemServiceMoq.Object);
+
+            try
+            {
+                await target.ClosePlaningPeriod(It.IsAny<string>(), It.IsAny<DateTime>());
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e.InnerException, typeof(ServiceException));
+            }
+        }
+    }
+}
---
+++ BussinessLogic/Managers/PlaningManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainModels.Model;
+using Services;
+using Services.Exceptions;
+using Services.Managers;
+
+namespace BussinessLogic.Managers
+{
+    public class PlaningManager: IPlaningManager
+    {
+        private readonly IPlanItemService _planItemService;
+
+        public PlaningManager(IPlanItemService planItemService)
+        {
+            _planItemService = planItemService;
+        }
+
+        public Task CalculatePlaningBalance(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ClosePlaningPeriod(string userId, DateTime date)
+        {
+            var previousDate = GetPreviousDate(date);
+            Func<PlanItem, bool> predicate = p =>
+                p.UserId == userId && p.Month.Month == previousDate.Month && p.Month.Year == previousDate.Year && p.Closed == false;
+
+            try
+            {
+                var previousItems = (await _planItemService.GetListAsync(userId))
+                    .Where(predicate)
+                    .ToList();
+
+                if (previousItems.Any())
+                {
+                    var firstPreviousItem = previousItems.FirstOrDefault();
+                    if (firstPreviousItem != null)
+                    {
+                        var previousBalanceFact = firstPreviousItem.BalanceFact;
+                        previousItems.ForEach(p =>
+                        {
+                            p.Closed = true;
+                            p.BalanceFact = previousBalanceFact;
+                        });
+                    }
+                }
+            }
+            catch (ServiceException e)
+            {
+                throw new ServiceException($"Ошибка в классе {nameof(PlaningManager)} в методе {nameof(ClosePlaningPeriod)} при вызове сервиса {_planItemService.GetType().Name}", e);
+            }
+        }
+ 
+        public Task<decimal> GetPlaningBalanceForMonth(string userId, DateTime month)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DateTime GetPreviousDate(DateTime date)
+        {
+            return date.Month == 1 ? new DateTime(date.Year - 1, 12, 1) : new DateTime(date.Year, date.Month - 1, 1);
+        }
+    }
+}
