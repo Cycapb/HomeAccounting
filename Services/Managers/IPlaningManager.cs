70b2c518c9f7d764da2fc02753e0049282fe921f
Susarev Andrey <aka.apostol@gmail.com>
2018-06-17 21:25:57 +0500

    Добавил интерфейс IPlaningManager в сборку Services. #HA-81 Developing

---
+++ Services/Managers/IPlaningManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Services.Managers
+{
+    /// <summary>
+    /// Интерфейс менеджера планирования
+    /// </summary>
+    public interface IPlaningManager
+    {
+
+        /// <summary>
+        /// Расчет баланса для планирования
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Task</returns>
+        Task CalculatePlaningBalance(string userId);
+
+        /// <summary>
+        /// Закрытие периода для планирования
+        /// </summary>
+        /// <param name="userId">ID пользователя</param>
+        /// <param name="date">Дата закрытия периода планирования</param>
+        /// <returns>Task</returns>
+        Task ClosePlaningPeriod(string userId, DateTime date);
+
+        /// <summary>
+        /// Получение планируемого баланса на заданный месяц
+        /// </summary>
+        /// <param name="userId">ID пользователя</param>
+        /// <param name="month">Месяц</param>
+        /// <returns>Асинхронно возвращает планируемый остаток на заданный месяц</returns>
+        Task<decimal> GetPlaningBalanceForMonth(string userId, DateTime month);
+    }
+}
