b676e73ed8f9d723e61b2cd9291c3bcce045e8a1
Cycapb <aka.apostol@gmail.com>
2019-03-16 12:16:54 +0500

    Updated many nuget packages #HA-88 Developing

--- WebUI/App_Start/FilterConfig.cs
+++ WebUI/App_Start/FilterConfig.cs
@@ -12,9 +12,9 @@ namespace WebUI
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new CustomErrorAttribute(
-                (IExceptionLogger) NinjectWebCommon.Kernel.GetService(typeof(IExceptionLogger)),
-                (IRouteDataConverter) NinjectWebCommon.Kernel.GetService(typeof(IRouteDataConverter)),
-                (IMultipleIpAddressProvider) NinjectWebCommon.Kernel.GetService(typeof(IMultipleIpAddressProvider))
+                (IExceptionLogger)NinjectWebCommon.Kernel.GetService(typeof(IExceptionLogger)),
+                (IRouteDataConverter)NinjectWebCommon.Kernel.GetService(typeof(IRouteDataConverter)),
+                (IMultipleIpAddressProvider)NinjectWebCommon.Kernel.GetService(typeof(IMultipleIpAddressProvider))
                 ));
         }
     }
