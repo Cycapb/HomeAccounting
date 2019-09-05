24bac045bbe7b1df00ed018c29bc18e7d5887d56
Cycapb <aka.apostol@gmail.com>
2019-09-05 23:44:04 +0500

    Added new type IPayingItemCreator and its implementation PayingItemCreator #HA-104 Developing

---
+++ WebUI/Abstract/IPayingItemCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Abstract
+{
+    public interface IPayingItemCreator
+    {
+        Task CreatePayingItem(PayingItemModel model);
+    }
+}
---
+++ WebUI/Implementations/PayingItemCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebUI.Abstract;
+using WebUI.Models;
+
+namespace WebUI.Implementations
+{
+    public class PayingItemCreator : IPayingItemCreator
+    {
+        public Task CreatePayingItem(PayingItemModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
