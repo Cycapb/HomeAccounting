b1c762e293370bad4e66f7754134e9f5ee6e5735
Cycapb <aka.apostol@gmail.com>
2019-09-15 20:39:40 +0500

    Added new intrface IPayingItemEditViewModelCreator

---
+++ WebUI/Abstract/IPayingItemEditViewModelCreator.cs
@@ -0,0 +1,11 @@
+﻿using DomainModels.Model;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Abstract
+{
+    public interface IPayingItemEditViewModelCreator
+    {
+        Task<PayingItemEditViewModel> CreateViewModel(PayingItem payingItem);
+    }
+}
