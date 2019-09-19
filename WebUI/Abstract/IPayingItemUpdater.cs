745b4ba4f5d013b5ca6745ef26bd16137788992e
Cycapb <aka.apostol@gmail.com>
2019-09-19 20:35:33 +0500

    Adde interface IPayingItemUpdater #HA-102 Developing

---
+++ WebUI/Abstract/IPayingItemUpdater.cs
@@ -0,0 +1,11 @@
+﻿using DomainModels.Model;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Abstract
+{
+    public interface IPayingItemUpdater
+    {
+        Task<PayingItem> UpdatePayingItemFromViewModel(PayingItemEditViewModel model);
+    }
+}
