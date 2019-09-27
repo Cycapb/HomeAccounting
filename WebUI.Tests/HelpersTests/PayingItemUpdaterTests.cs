a1b19a40fab25939f129c6a3fd4e02446c7ed1ee
Cycapb <aka.apostol@gmail.com>
2019-09-27 21:58:31 +0500

    Wrote SetCorrectSumFromViewModelIfProductsInItemAreNotNull, SetCorrectCommentFromViewModelIfProductsInItemAreNotNull unit tests #HA-102 Developing

--- WebUI.Tests/HelpersTests/PayingItemUpdaterTests.cs
+++ WebUI.Tests/HelpersTests/PayingItemUpdaterTests.cs
@@ -78,5 +78,72 @@ namespace WebUI.Tests.HelpersTests
             Assert.AreEqual("CommentFromViewModel", result.Comment);
         }
 
+        [TestMethod]
+        [TestCategory("PayingItemUpdaterTests")]
+        public async Task SetCorrectSumFromViewModelIfProductsInItemAreNotNull()
+        {
+            var payingItem = new PayingItem()
+            {
+                ItemID = 1
+            };
+            _payinItemServiceMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(payingItem);
+            var target = new PayingItemUpdater(_payinItemServiceMock.Object);
+            var payingItemViewModel = CreatePayingItemViewModelWithProductsInItem();
+
+            var result = await target.UpdatePayingItemFromViewModel(payingItemViewModel);
+
+            Assert.AreEqual(300, result.Summ);
+        }
+
+        [TestMethod]
+        [TestCategory("PayingItemUpdaterTests")]
+        public async Task SetCorrectCommentFromViewModelIfProductsInItemAreNotNull()
+        {
+            var payingItem = new PayingItem()
+            {
+                ItemID = 1
+            };
+            _payinItemServiceMock.Setup(x => x.GetItemAsync(It.IsAny<int>())).ReturnsAsync(payingItem);
+            var target = new PayingItemUpdater(_payinItemServiceMock.Object);
+            var payingItemViewModel = CreatePayingItemViewModelWithProductsInItem();
+
+            var result = await target.UpdatePayingItemFromViewModel(payingItemViewModel);
+
+            Assert.AreEqual("P1, P2, P3", result.Comment);
+        }
+
+        private PayingItemEditViewModel CreatePayingItemViewModelWithProductsInItem()
+        {
+            return new PayingItemEditViewModel()
+            {
+                PayingItem = new PayingItem()
+                {
+                    ItemID = 1,
+                    Summ = 500M,
+                    Comment = "CommentFromViewModel"
+                },
+                ProductsInItem = new System.Collections.Generic.List<Product>()
+                {
+                    new Product()
+                    {
+                        ProductID = 1,
+                        ProductName = "P1",
+                        Price = 100
+                    },
+                    new Product()
+                    {
+                        ProductID = 2,
+                        ProductName = "P2",
+                        Price = 100
+                    },
+                    new Product()
+                    {
+                        ProductID = 1,
+                        ProductName = "P3",
+                        Price = 100
+                    }
+                }
+            };
+        }
     }
 }
