f4089a8d10cf5d498eb47be00ccd96bf789a9566
Cycapb <aka.apostol@gmail.com>
2019-12-22 14:35:52 +0500

    Added new interface IServiceAsync to Services.dll assymbly #HA-105 Developing

---
+++ Services/IServiceAsync.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IServiceAsync<T> where T : class
+    {
+        Task<IEnumerable<T>> GetListAsync();
+
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> predicate);
+    }
+}
