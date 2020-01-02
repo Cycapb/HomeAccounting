428b679eafc1a0280123c84ec7f8cc69d5c64b69
Cycapb <aka.apostol@gmail.com>
2020-01-03 01:08:13 +0500

    Added composite primary key to OrderDetail and created migration for it #HA-115 Developing

--- DomainModels/Model/OrderDetail.cs
+++ DomainModels/Model/OrderDetail.cs
@@ -1,12 +1,18 @@
 namespace DomainModels.Model
 {
+    using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
     [Table("OrderDetail")]
     public partial class OrderDetail
     {
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
+        [Key]
+        [Column(Order = 2)]
         public int OrderId { get; set; }
 
         public int ProductId { get; set; }
