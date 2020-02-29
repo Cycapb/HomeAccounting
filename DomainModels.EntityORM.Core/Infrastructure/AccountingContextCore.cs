52268b7a53aa628431ce18a46cb8a95f994706e8
Cycapb <aka.apostol@gmail.com>
2020-02-29 16:54:00 +0500

    Added dbsets for all entities into the AccountingContextCore #HA-124 Developing

--- DomainModels.EntityORM.Core/Infrastructure/AccountingContextCore.cs
+++ DomainModels.EntityORM.Core/Infrastructure/AccountingContextCore.cs
@@ -8,11 +8,16 @@ namespace DomainModels.EntityORM.Core.Infrastructure
         public AccountingContextCore(DbContextOptions<AccountingContextCore> options) : base(options)
         {
         }
-
-        public DbSet<Account> Accounts { get; set; }
-
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Debt> Debts { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Debt> Debts { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<PayingItem> PayingItems { get; set; }
+        public virtual DbSet<PlanItem> PlanItems { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<TypeOfFlow> TypeOfFlows { get; set; }
+        public virtual DbSet<NotificationMailBox> NotificationMailBoxes { get; set; }
+        public virtual DbSet<PayingItemProduct> PayingItemProducts { get; set; }
     }
 }
