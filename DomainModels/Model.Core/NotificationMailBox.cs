4bd788a5617c38c2948aaf43f8bb650cf8100b32
Cycapb <aka.apostol@gmail.com>
2020-02-29 15:49:20 +0500

    Added new model NotificationMailBox to Model.Core #HA-124 Developing

---
+++ DomainModels/Model.Core/NotificationMailBox.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels.Model.Core
+{
+    [Table("NotificationMailBox")]
+    public class NotificationMailBox
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string MailBoxName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string MailFrom { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(1024)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Server { get; set; }
+        [Required]
+        public int Port { get; set; }
+        public bool UseSsl { get; set; } = true;
+    }
+}
