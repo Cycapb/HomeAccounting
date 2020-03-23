286500fcce6d7dd41eca6d5d5dde607c768a0d5e
Cycapb <aka.apostol@gmail.com>
2020-03-23 12:47:24 +0500

    Added SessionExtension class with extension methods for Session #HA-119 Developing

---
+++ WebUI.Core/Infrastructure/Extensions/SessionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace WebUI.Core.Infrastructure.Extensions
+{
+    public static class SessionExtension
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.LoadAsync();
+            session.Set(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            session.LoadAsync();
+            var item = session.GetString(key);
+
+            return item == null ? default : JsonConvert.DeserializeObject<T>(item);
+        }
+    }
+}
