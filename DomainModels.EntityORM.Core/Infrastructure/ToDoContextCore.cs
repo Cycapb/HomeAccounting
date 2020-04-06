c0235cffbc39d7ba874f5cba67d30cd8db81d5e3
Cycapb <aka.apostol@gmail.com>
2020-04-06 22:10:59 +0500

    Added ToDoContextCore.cs to DomainModels.EntityORM.Core project #HA-119 Developing

---
+++ DomainModels.EntityORM.Core/Infrastructure/ToDoContextCore.cs
@@ -0,0 +1,17 @@
+﻿using DomainModels.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainModels.EntityORM.Core.Infrastructure
+{
+    public class ToDoContextCore : DbContext
+    {
+        public ToDoContextCore(DbContextOptions<ToDoContextCore> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public virtual DbSet<TodoGroup> TodoGroups { get; set; }
+
+        public virtual DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
