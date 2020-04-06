4ba0f48d52608822b929e6840493c1ec7ff39ad8
Cycapb <aka.apostol@gmail.com>
2020-04-06 21:54:21 +0500

    Added new project ToDoList.Core. Added new models to DomainModel - TodoItem and TodoGroup #HA-119 Developing

---
+++ DomainModels/Model/TodoGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels.Model
+{
+    [Table(nameof(TodoGroup))]
+    public class TodoGroup
+    {
+        public TodoGroup()
+        {
+            TodoItems = new HashSet<TodoItem>();
+        }
+
+        public int Id { get; set; }
+                
+        [StringLength(50)]
+        [Required(ErrorMessage = "Не задано имя группы")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не задан пользователь")]
+        public string UserId { get; set; }
+
+        public virtual ICollection<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ DomainModels/Model/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels.Model
+{
+    [Table(nameof(TodoItem))]
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "У задачи должно быть описание")]
+        [StringLength(250)]
+        public string Description { get; set; }
+
+        public int GroupId { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        [Required(ErrorMessage = "Не задан пользователь")]
+        public string UserId { get; set; }
+
+        public virtual TodoGroup Group { get; set; }
+    }
+}
