682e5fc5150c91ff726ff0059ba0a00ddeb3e887
Cycapb <aka.apostol@gmail.com>
2020-06-17 11:35:20 +0500

    Write logic of getting collection of accounts in NavLeft viewcomponent. Added invokation of this viewcomponent in Views\PayingItem\_MainPage.cshtml #HA-134 Developing

--- WebUI.Core/Components/NavLeft.cs
+++ WebUI.Core/Components/NavLeft.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DomainModels.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Core.Exceptions;
+using WebUI.Core.Infrastructure.Extensions;
+using WebUI.Core.Models;
 
 namespace WebUI.Core.Components
 {
     public class NavLeft : ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {            
-            return View("/Views/NavLeft/Accounts.cshtml");
+        private readonly IAccountService _accountService;
+
+        public NavLeft(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                var accounts = new List<Account>();
+                var user = await HttpContext.Session.GetJsonAsync<WebUser>("WebUser");
+                
+                if (user != null)
+                {
+                    var taskResult = await _accountService.GetListAsync(u => u.UserId == user.Id);
+                    accounts = taskResult.ToList();
+                }
+
+                return View("/Views/NavLeft/_Accounts.cshtml", accounts);
+            }
+            catch (ServiceException e)
+            {
+                throw new WebUiException($"Ошибка в компоненте {nameof(NavLeft)} в методе {nameof(InvokeAsync)}", e);
+            }
+            catch (Exception ex)
+            {
+                throw new WebUiException($"Ошибка в компоненте {nameof(NavLeft)} в методе {nameof(InvokeAsync)}", ex);
+            }
         }
     }
 }
