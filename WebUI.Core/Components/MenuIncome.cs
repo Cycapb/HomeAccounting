4edf8ef281d4fc01e7df9e27bb7848778a783183
Cycapb <aka.apostol@gmail.com>
2020-07-20 22:20:45 +0500

    Added MenuIncome.cs ViewComponent and its invoking in _MainPage.cshtml. #HA-134 Developing

---
+++ WebUI.Core/Components/MenuIncome.cs
@@ -0,0 +1,63 @@
+﻿using DomainModels.Model;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Core.Abstract;
+using WebUI.Core.Exceptions;
+using WebUI.Core.Infrastructure.Extensions;
+using WebUI.Core.Models;
+using WebUI.Core.Models.BudgetModels;
+using WebUI.Core.Models.Enums;
+
+namespace WebUI.Core.Components
+{
+    public class MenuIncome : ViewComponent
+    {
+        private readonly IPayingItemService _payingItemService;
+        private readonly IReportHelper _reportHelper;
+
+        public MenuIncome(IPayingItemService payingItemService, IReportHelper reportHelper)
+        {
+            _payingItemService = payingItemService;
+            _reportHelper = reportHelper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            try
+            {
+                var user = await ViewComponentContext.ViewContext.HttpContext.Session.GetJsonAsync<WebUser>(nameof(WebUser));
+                var collection = new List<PayingItem>();
+
+                if (user != null)
+                {
+                    collection = _payingItemService.GetListByTypeOfFlow(user, (int)TypesOfFlow.Income).ToList();
+                }
+
+                var budgetModel = BudgetModel(collection, TypesOfFlow.Income);
+
+                return View("/Views/NavRight/_MenuIncoming.cshtml", budgetModel);
+            }
+            catch (ServiceException e)
+            {
+                throw new WebUiException($"Ошибка в ViewComponent с названием {nameof(MenuIncome)} в методе {nameof(InvokeAsync)}", e);
+            }
+        }
+
+        private BudgetModel BudgetModel(List<PayingItem> collection, TypesOfFlow typeOfFlow)
+        {
+            var budget = new BudgetModel()
+            {
+                TypeOfMoney = typeOfFlow == TypesOfFlow.Income ? "Доход" : "Расход",
+                Month = _reportHelper.GetSummForMonth(collection),
+                Week = _reportHelper.GetSummForWeek(collection),
+                Day = _reportHelper.GetSummForDay(collection)
+            };
+
+            return budget;
+        }
+    }
+}
