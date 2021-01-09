e4f964b5faa0a11d77a519e6a23adf53586832fd
Cycapb <aka.apostol@gmail.com>
2021-01-09 15:58:40 +0500

    Removed unused usings from ProductControllerTests.cs. #HA-150 Developing

--- WebUI.Core.Tests/Controllers/ProductControllerTests.cs
+++ WebUI.Core.Tests/Controllers/ProductControllerTests.cs
@@ -6,7 +6,6 @@ using Services;
 using Services.Exceptions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using WebUI.Core.Controllers;
