cf5cab7bf060180758e5d4033ea5c4c4c5b664bc
Cycapb <aka.apostol@gmail.com>
2021-01-16 13:47:36 +0500

    Renamed NavLeftControllerTests into AccountingInformationControllerTests and fixed tests in it. #HA-154 Developing

---
+++ WebUI.Core.Tests/Controllers/AccountingInformationControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using DomainModels.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Services;
+using Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WebUI.Controllers;
+using WebUI.Core.Exceptions;
+using WebUI.Core.Models;
+
+namespace WebUI.Tests.ControllersTests
+{
+    [TestClass]
+    public class AccountingInformationControllerTests
+    {
+        private readonly Mock<IAccountService> _accountServiceMock;
+        private List<Account> _accounts => new List<Account>()
+            {
+                new Account() {AccountID = 1,AccountName = "Acc 1", Cash = 100M, UserId = "1"},
+                new Account() {AccountID = 2,AccountName = "Acc 2", Cash = 200M, UserId = "1"},
+                new Account() {AccountID = 3,AccountName = "Acc 3", Cash = 1000M, UserId = "1"},
+            };
+
+        public AccountingInformationControllerTests()
+        {
+            _accountServiceMock = new Mock<IAccountService>();
+        }
+
+        [TestMethod]
+        [TestCategory("AccountingInformationControllerTests")]
+        public async Task Can_Get_Accounts_By_UserId()
+        {
+            var userId = "1";
+            var account = new Account() { UserId = userId };
+            _accountServiceMock.Setup(m => m.GetListAsync(It.Is<Expression<Func<Account, bool>>>(x => x.Compile()(account)))).ReturnsAsync(_accounts.Where(x => x.UserId == userId));
+            var target = new AccountingInformationController(_accountServiceMock.Object, null, null);
+
+            var result = await target.Accounts(new WebUser() { Id = "1" });
+            var model = (((PartialViewResult)result).Model as IEnumerable<Account>)?.ToList();
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(3, model?.Count);
+            Assert.AreEqual(1, model?[0].AccountID);
+            Assert.AreEqual(2, model?[1].AccountID);
+            Assert.AreEqual(3, model?[2].AccountID);
+        }
+
+        [TestMethod]
+        [TestCategory("AccountingInformationControllerTests")]
+        [ExpectedException(typeof(WebUiException))]
+        public async Task Accounts_RaisesWebUiException()
+        {
+            _accountServiceMock.Setup(m => m.GetListAsync(It.IsAny<Expression<Func<Account, bool>>>())).Throws<ServiceException>();
+            var target = new AccountingInformationController(_accountServiceMock.Object, null, null);
+
+            await target.Accounts(new WebUser());
+        }
+    }
+}
---
+++ WebUI.Core.Tests/Controllers/NavRightControllertTests.cs
@@ -0,0 +1,88 @@
+﻿//using System.Collections.Generic;
+//using System.Web.Mvc;
+//using DomainModels.Model;
+//using WebUI.Controllers;
+//using WebUI.Models;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Moq;
+//using Services;
+//using Services.Exceptions;
+//using WebUI.Exceptions;
+//using WebUI.Abstract;
+//using WebUI.Models.BudgetModels;
+
+//namespace WebUI.Tests.ControllersTests
+//{
+//    [TestClass]
+//    public class NavRightControllertTests
+//    {
+//        private readonly Mock<IPayingItemService> _service;
+//        private readonly Mock<IReportHelper> _dbHelper;
+
+
+//        public NavRightControllertTests()
+//        {
+//            _service = new Mock<IPayingItemService>();
+//            _dbHelper = new Mock<IReportHelper>();
+//        }
+
+
+//        [TestMethod]
+//        [TestCategory("NavRightControllertTests")]
+//        public void GetIncomingBudget()
+//        {
+//            _service.Setup(m => m.GetListByTypeOfFlow(It.IsAny<IWorkingUser>(),It.IsAny<int>())).Returns(new List<PayingItem>());
+//            _dbHelper.Setup(m => m.GetSummForDay(It.IsAny<List<PayingItem>>())).Returns(500);
+//            _dbHelper.Setup(m => m.GetSummForMonth(It.IsAny<List<PayingItem>>())).Returns(300);
+//            _dbHelper.Setup(m => m.GetSummForWeek(It.IsAny<List<PayingItem>>())).Returns(1000);
+//            var target = new NavRightController(_service.Object, _dbHelper.Object);
+
+//            var result = ((PartialViewResult)target.MenuIncoming(new WebUser())).Model as BudgetModel;
+
+//            Assert.AreEqual(500, result.Day);
+//            Assert.AreEqual(300, result.Month);
+//            Assert.AreEqual(1000, result.Week);
+//        }
+
+//        [TestMethod]
+//        [TestCategory("NavRightControllertTests")]
+//        public void GetOutgoBudget()
+//        {
+//            _service.Setup(m => m.GetListByTypeOfFlow(It.IsAny<IWorkingUser>(),It.IsAny<int>())).Returns(new List<PayingItem>());
+//            var target = new NavRightController(_service.Object, _dbHelper.Object);
+//            _dbHelper.Setup(m => m.GetSummForDay(It.IsAny<List<PayingItem>>())).Returns(1000);
+//            _dbHelper.Setup(m => m.GetSummForMonth(It.IsAny<List<PayingItem>>())).Returns(1500);
+//            _dbHelper.Setup(m => m.GetSummForWeek(It.IsAny<List<PayingItem>>())).Returns(1300);
+
+//            var result = ((PartialViewResult)target.MenuOutgo(new WebUser())).Model as BudgetModel; 
+
+//            Assert.AreEqual(1000, result.Day);
+//            Assert.AreEqual(1300, result.Week);
+//            Assert.AreEqual(1500, result.Month);
+//        }
+
+//        [TestMethod]
+//        [TestCategory("NavRightControllertTests")]
+//        [ExpectedException(typeof(WebUiException))]
+//        public void MenuIncoming_RaisesWebUiException()
+//        {
+//            _service.Setup(m => m.GetListByTypeOfFlow(It.IsAny<IWorkingUser>(), It.IsAny<int>()))
+//                .Throws<ServiceException>();
+//            var target = new NavRightController(_service.Object, null);
+
+//            target.MenuIncoming(new WebUser());
+//        }
+
+//        [TestMethod]
+//        [TestCategory("NavRightControllertTests")]
+//        [ExpectedException(typeof(WebUiException))]
+//        public void MenuOutgo_RaisesWebUiException()
+//        {
+//            _service.Setup(m => m.GetListByTypeOfFlow(It.IsAny<IWorkingUser>(), It.IsAny<int>()))
+//                .Throws<ServiceException>();
+//            var target = new NavRightController(_service.Object, null);
+
+//            target.MenuOutgo(new WebUser());
+//        }
+//    }
+//}
