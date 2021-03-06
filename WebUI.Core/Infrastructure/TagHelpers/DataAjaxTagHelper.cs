a0fa7f760018f55dfccb2183a590dd7fc3a84d79
Cycapb <aka.apostol@gmail.com>
2021-03-06 14:18:23 +0500

    Add DataAjaxTagHelper to WebUI.Core\Infrastructure namespace #HA-176 Developing

---
+++ WebUI.Core/Infrastructure/TagHelpers/DataAjaxTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebUI.Core.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "[ajax-enable=true]")]
+    [HtmlTargetElement("form", Attributes = "[ajax-enable=true]")]
+    public class DataAjaxTagHelper : TagHelper
+    {
+        public bool AjaxEnable { get; set; }
+
+        public string AjaxMode { get; set; }
+
+        public string AjaxUpdateElementId { get; set; }
+
+        public string AjaxUrl { get; set; }
+
+        public string AjaxSuccess { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("data-ajax", "true");
+            
+            if (string.IsNullOrWhiteSpace(AjaxMode))
+            {
+                output.Attributes.Add("data-ajax-mode", "replace");
+            }
+            else
+            {
+                output.Attributes.Add("data-ajax-mode", AjaxMode);
+            }
+
+            output.Attributes.Add("data-ajax-update", AjaxUpdateElementId);
+            output.Attributes.Add("data-ajax-url", AjaxUrl);
+
+            if (!string.IsNullOrWhiteSpace(AjaxSuccess))
+            {
+                output.Attributes.Add("data-ajax-success", AjaxSuccess);
+            }
+        }
+    }
+}
