a557a67cf83de5764dc71edb31f5df5d530844a6
Cycapb <aka.apostol@gmail.com>
2021-03-20 12:23:39 +0500

    Add ReportController, ReportControllerHelper,Paginator.dll, ReportModel to WebUI.Core #HA-167 Developing

--- WebUI.Core/Models/ReportModels/ReportModel.cs
+++ WebUI.Core/Models/ReportModels/ReportModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using WebUI.Core.Models.CategoryModels;
-using WebUI.Core.Models.ReportModels;
 
 namespace WebUI.Core.Models.ReportModels
 {
