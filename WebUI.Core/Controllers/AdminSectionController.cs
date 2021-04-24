6f8766176ea5c9839352119ed3dbfda0defecf3e
Cycapb <aka.apostol@gmail.com>
2021-04-24 19:41:15 +0500

    Add AdminSectionController.cs. #HA-158 Developing

---
+++ WebUI.Core/Controllers/AdminSectionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Core.Controllers
+{
+    [Authorize(Roles = "Administrators")]
+    public class AdminSectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView("_Index");
+        }
+    }
+}
