d68c50568f3139e11975b2a3ea27de3f2562f5d2
Andrey Susarev <aka.apostol@gmail.com>
2022-06-30 13:54:50 +0500

    Fix tests in BusinnesLogic.Tests and WebUI.Core.Tests projects #HA-185 Developing
    - Migrate BusinessLogic.Tests to Net 6.0

--- BusinessLogic.Tests/ServicesTests/CreateCloseDebtServiceTests.cs
+++ BusinessLogic.Tests/ServicesTests/CreateCloseDebtServiceTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
 using BussinnessLogic.Services;
 using DomainModels.Exceptions;
